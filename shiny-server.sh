117f79ea7dfd67767949110cdc7a395cc1607ffb
shrektan <shrektan@126.com>
2015-09-14 00:36:20 +0800

    Make RStudioServer & ShinyServer Run Together

--- shiny-server.sh
+++ shiny-server.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+# RUN ShinyServer
+
 # Make sure the directory for individual app logs exists
 mkdir -p /var/log/shiny-server
-chown shiny.shiny /srv/shiny-server # should have the writing access by default
+sudo chown -R shiny /srv/shiny-server/* # should have the writing access by default
 chown shiny.shiny /var/log/shiny-server
 
 exec shiny-server >> /var/log/shiny-server.log 2>&1
+
+# RUN RStudioServer
+
+sudo mkdir -p /var/log/supervisor \
+	  && chgrp staff /var/log/supervisor \
+	  && chmod g+w /var/log/supervisor \
+	  && chgrp staff /etc/supervisor/conf.d/supervisord.conf
+
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
